96971809d694076a71ca84743650384446ddbd03
Valur Páll Stefán Valsson <32470206+valurp@users.noreply.github.com>
2019-03-27 13:29:43 +0000

    Add files via upload

---
+++ Hotels/src/utlit/HotelinterfaceController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utlit;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Valur Páll Stefán Valsson <vpv1@hi.is>
+ */
+public class HotelinterfaceController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
