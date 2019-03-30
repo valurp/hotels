48410b094bf59b170fd038068c0d00043edefdea
Þorgeir <thorgeir93@gmail.com>
2019-03-30 18:56:40 +0000

    Drög að getAvailableHotels

--- Hotels/src/vinnsla/API.java
+++ Hotels/src/vinnsla/API.java
@@ -7,24 +7,34 @@ import java.sql.*;
  */
 public class API {
     
-    public static void main(String[] args) throws SQLException {
+    private Connection connection;
+    private Statement statement;
+    private ResultSet result;
 
-        Connection connection = null;
-        Statement statement = null;
-        ResultSet result = null;
+    private String user;
+    private String pass;
+    
+    public API() {
+        connection = null;
+        statement = null;
+        result = null;
 
-        String user = "user";
-        String pass = "user";
-
+        user = "user";
+        pass = "user";
+    }
+    
+    /*Drög að getAvailableHotels, tekur núna bara við streng sem fyrirspurnin og nafn dálksins sem á að  prenta og prentar svo út í output*/
+    public void getAvailableHotels(String query, String queryResult) throws SQLException {
+        
         try {
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotelsearch", user, pass);
 
             statement = connection.createStatement();
 
-            result = statement.executeQuery("select * from hotels");
+            result = statement.executeQuery(query);
 
             while (result.next()) {
-                System.out.println(result.getString("name"));
+                System.out.println(result.getString(queryResult));
             }
 
         } catch (Exception exc) {
@@ -42,5 +52,13 @@ public class API {
                 connection.close();
             }
         }
+    }
+    
+    //tímabundið main fall hér, bara til að prófa
+    public static void main(String[] args) throws SQLException {
+        API api = new API();
+        //Dæmi um fyrirspurn
+        api.getAvailableHotels("SELECT * FROM hotels", "name");
+        
     }    
 }
