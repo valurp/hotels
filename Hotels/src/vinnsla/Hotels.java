7fd268e6d6c80211d481bc4d852e8a8e9ebdb3ec
Valur Páll Stefán Valsson <32470206+valurp@users.noreply.github.com>
2019-03-31 14:14:56 +0000

    Add files via upload
    Bjó til smá interface og færði hotelinterface.fxml yfir í vinnslu package-ið til þess að fá það til að keyra

--- Hotels/src/vinnsla/Hotels.java
+++ Hotels/src/vinnsla/Hotels.java
@@ -9,8 +9,14 @@ package vinnsla;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.RadioButton;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 /**
@@ -20,33 +26,33 @@ import javafx.stage.Stage;
  */
 public class Hotels extends Application {
 
+    @FXML
+    private DatePicker arrDate;
+    @FXML
+    private DatePicker depDate;
+    @FXML
+    private MenuButton Cities;
+    @FXML
+    private RadioButton breakfast;
+    @FXML
+    private Button searchbutton;
+
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Hello World!");
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("hotelinterface.fxml"));
+        
+        Scene scene = new Scene(root);
+        
         primaryStage.setScene(scene);
         primaryStage.show();
+        primaryStage.setResizable(false);
     }
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
     }
-
 }
+
