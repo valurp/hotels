1edb6920e9509e8c9bea593e3a63ec775b43c157
valurp <vpv1@hi.is>
2019-04-05 15:16:25 +0000

    bætti við interface

--- Hotels/src/utlit/HotelinterfaceController.java
+++ Hotels/src/utlit/HotelinterfaceController.java
@@ -7,7 +7,13 @@ package utlit;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioButton;
 
 /**
  * FXML Controller class
@@ -15,6 +21,29 @@ import javafx.fxml.Initializable;
  * @author Valur Páll Stefán Valsson <vpv1@hi.is>
  */
 public class HotelinterfaceController implements Initializable {
+
+    @FXML
+    private DatePicker arrDate;
+    @FXML
+    private DatePicker depDate;
+    @FXML
+    private MenuButton Cities;
+    @FXML
+    private Button searchbutton;
+    @FXML
+    private RadioButton breakfast;
+    @FXML
+    private MenuItem akureyri;
+    @FXML
+    private MenuItem reykjavik;
+    @FXML
+    private MenuItem borgarnes;
+    @FXML
+    private MenuItem husavik;
+    @FXML
+    private MenuItem keflavik;
+    @FXML
+    private MenuItem egilsstadir;
 
     /**
      * Initializes the controller class.
