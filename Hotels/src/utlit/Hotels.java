fad8eebbdc92895078e5744de7bbd5c36acb13e3
valurp <vpv1@hi.is>
2019-04-07 23:17:09 +0000

    uppfærsla

--- Hotels/src/utlit/Hotels.java
+++ Hotels/src/utlit/Hotels.java
@@ -8,6 +8,8 @@ package utlit;
 
 import java.util.ArrayList;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -15,11 +17,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.RadioButton;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import vinnsla.SearchPageController;
 /**
  *
  * @author Valur Páll Stefán Valsson <vpv1@hi.is>
@@ -35,9 +41,23 @@ public class Hotels extends Application {
     @FXML
     private MenuButton Cities;
     @FXML
-    private RadioButton breakfast;
+    private CheckBox breakfastButton;
     @FXML
     private Button searchbutton;
+    @FXML
+    private MenuItem akureyri;
+    @FXML
+    private MenuItem reykjavik;
+    @FXML
+    private MenuItem keflavik;
+    @FXML
+    private MenuItem egilsstadir;
+    
+    public boolean breakfast;
+
+    @FXML
+    public ListView<String> results;
+    
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -52,9 +72,59 @@ public class Hotels extends Application {
     
     /**
      * @param args the command line arguments
+     * Controller fyrir interface-ið
      */
     public static void main(String[] args) {
         launch(args);
     }
-}
+    
+    /**
+     * TODO:
+     * Fá niðurstöður frá SearchPageController og birta í results ListView-inu
+     *
+     */
+    
+
+    @FXML
+    private void searchHandler(ActionEvent event) {
+        
+    }
+
+    @FXML
+    private void arrDateHandler(ActionEvent event) {
+        
+    }
+
+    @FXML
+    private void depDateHandler(ActionEvent event) {
+        
+    }
+
+    @FXML
+    private void akSelect(ActionEvent event) {
+        Cities.setText("Akureyri");
+    }
+
+    @FXML
+    private void rvkSelect(ActionEvent event) {
+        Cities.setText("Reykjavík");
+    }
+
+    @FXML
+    private void kefSelect(ActionEvent event) {
+        Cities.setText("Keflavík");
+    }
+
+    @FXML
+    private void egSelect(ActionEvent event) {
+        Cities.setText("Egilsstaðir");
+    }
+
+    @FXML
+    private void breakfastButtonHandler(ActionEvent event) {
+        breakfast = breakfastButton.isSelected();
+    }
+    
+    
+    }
 
